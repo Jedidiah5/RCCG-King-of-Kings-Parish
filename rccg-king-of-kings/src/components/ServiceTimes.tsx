6b31b13dafac42def749e650d9b835b8042bae5c
enesi <jedidiahonotu@gmail.com>
2025-06-14 16:48:47 +0100

    fixed deployment issues

--- rccg-king-of-kings/src/components/ServiceTimes.tsx
+++ rccg-king-of-kings/src/components/ServiceTimes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-// import { ClockIcon } from '@heroicons/react/24/outline';
+// import { CalendarIcon, ClockIcon, MapPinIcon } from '@heroicons/react/24/outline';
 
 const ServiceTimes = () => {
   const services = [
