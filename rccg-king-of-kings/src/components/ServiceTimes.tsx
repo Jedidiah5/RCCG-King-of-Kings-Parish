088b6bbe184aeb0bab898072d2bdca1c7b63faa6
enesi <jedidiahonotu@gmail.com>
2025-06-14 16:34:22 +0100

    fixed clock icons

--- rccg-king-of-kings/src/components/ServiceTimes.tsx
+++ rccg-king-of-kings/src/components/ServiceTimes.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CalendarIcon, ClockIcon, MapPinIcon } from '@heroicons/react/24/outline';
+import { ClockIcon } from '@heroicons/react/24/outline';
 
 const ServiceTimes = () => {
   const services = [
